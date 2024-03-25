9cbc3997ee6e7773d537817db252123f7876eeab
chanmin-00 <jjw421412@naver.com>
2024-03-25 17:19:21 +0900

    :memo: service 생성 및 추가

---
+++ src/main/java/ssuPlector/service/DeveloperService.java
@@ -0,0 +1,9 @@
+package ssuPlector.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeveloperService {
+}
