5a13ad2846f99e850662232ec6831536cd3387d3
chanmin-00 <jjw421412@naver.com>
2024-03-25 17:19:02 +0900

    :memo: controller 생성 및 추가

---
+++ src/main/java/ssuPlector/controller/DeveloperController.java
@@ -0,0 +1,11 @@
+package ssuPlector.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/developers")
+public class DeveloperController {
+}
