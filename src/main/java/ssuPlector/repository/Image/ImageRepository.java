b5039586d8a8ec303debfd9283fe64430f538468
Ares-10 <cholionking1@naver.com>
2024-03-26 16:39:24 +0900

    refactor: 코드 컨벤션 적용

--- src/main/java/ssuPlector/repository/Image/ImageRepository.java
+++ src/main/java/ssuPlector/repository/Image/ImageRepository.java
@@ -2,8 +2,8 @@ package ssuPlector.repository.Image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import ssuPlector.domain.Image;
 
 @Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
+public interface ImageRepository extends JpaRepository<Image, Long> {}
--- src/main/java/ssuPlector/repository/project/ProjectRepository.java
+++ src/main/java/ssuPlector/repository/project/ProjectRepository.java
@@ -2,8 +2,8 @@ package ssuPlector.repository.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import ssuPlector.domain.Project;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {
-}
+public interface ProjectRepository extends JpaRepository<Project, Long>, ProjectRepositoryCustom {}
