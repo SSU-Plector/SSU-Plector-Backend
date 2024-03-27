34b35b8aaee45b9506f5aac64db4932bdf2977dd
88dldl <chltnrgml0999@gmail.com>
2024-03-27 13:16:14 +0900

    :sparkles: [Feat] 예외 처리 핸들러 추가

--- src/main/java/ssuPlector/global/exception/ExceptionAdvice.java
+++ src/main/java/ssuPlector/global/exception/ExceptionAdvice.java
@@ -1,12 +1,19 @@
 package ssuPlector.global.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 import ssuPlector.global.response.ApiResponse;
+import ssuPlector.global.response.code.GlobalErrorCode;
 
 @Slf4j
 @RestControllerAdvice(annotations = {RestController.class})
@@ -17,6 +24,31 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     protected ApiResponse handleGlobalException(GlobalException e) {
         log.error(e.getGlobalErrorCode() + ": " + e.getMessage());
         return ApiResponse.onFailure(e.getGlobalErrorCode());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
+        String errorMessage =
+                e.getConstraintViolations().stream()
+                        .map(ConstraintViolation::getMessage)
+                        .findFirst()
+                        .orElseThrow(
+                                () ->
+                                        new RuntimeException(
+                                                "ConstraintViolationException 추출 도중 에러 발생"));
+
+        return handleExceptionInternalConstraint(
+                e, GlobalErrorCode.valueOf(errorMessage), HttpHeaders.EMPTY, request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternalConstraint(
+            Exception e,
+            GlobalErrorCode errorCommonStatus,
+            HttpHeaders headers,
+            WebRequest request) {
+        ApiResponse<Object> body = ApiResponse.onFailure(errorCommonStatus, null);
+        return super.handleExceptionInternal(
+                e, body, headers, errorCommonStatus.getHttpStatus(), request);
     }
 
     /** MethodArgumentNotValidException 등의 추가 예외 처리 핸들러 작성 필요 */
