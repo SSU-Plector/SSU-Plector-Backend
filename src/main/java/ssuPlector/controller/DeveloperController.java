de940fbfeff14e32f74992fcd0ff846e605acac6
88dldl <chltnrgml0999@gmail.com>
2024-04-03 15:23:18 +0900

    :memo: [Docs] API 명세 담당자 추가

--- src/main/java/ssuPlector/controller/DeveloperController.java
+++ src/main/java/ssuPlector/controller/DeveloperController.java
@@ -29,8 +29,7 @@ import ssuPlector.validation.annotation.ExistDeveloper;
 public class DeveloperController {
     private final DeveloperService developerService;
 
-    // 개발자 생성
-    @Operation(summary = "개발자 생성, 저장 API", description = "개발자를 생성합니다.")
+    @Operation(summary = "개발자 생성, 저장 API", description = "개발자를 생성합니다._찬민")
     @PostMapping
     public ApiResponse createDeveloper(@RequestBody @Valid DeveloperRequestDTO requestDTO) {
         Long developerId = developerService.createDeveloper(requestDTO);
