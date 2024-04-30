25f6893b7994f6ab18e76b28ba51f455416a7143
88dldl <chltnrgml0999@gmail.com>
2024-04-30 23:09:44 +0900

    :recycle: [Refactor] 컨벤션 적용

--- src/main/java/ssuPlector/controller/DeveloperController.java
+++ src/main/java/ssuPlector/controller/DeveloperController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import ssuPlector.converter.DeveloperConverter;
