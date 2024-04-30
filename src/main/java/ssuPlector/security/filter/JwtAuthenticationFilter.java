0485e342c22d0385d7f8d9aa91aef08d200ca069
88dldl <chltnrgml0999@gmail.com>
2024-04-30 22:53:16 +0900

    :sparkles: [Feat] Jwt 생성 및 유효성 확인

---
+++ src/main/java/ssuPlector/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package ssuPlector.security.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+import ssuPlector.global.exception.GlobalException;
+import ssuPlector.global.response.code.GlobalErrorCode;
+import ssuPlector.security.principal.PrincipalDetailsService;
+import ssuPlector.security.provider.JwtTokenProvider;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PrincipalDetailsService principalDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            String token = authorization.substring(7);
+            if (jwtTokenProvider.isValidToken(token)) {
+                Long userId = jwtTokenProvider.getId(token);
+                UserDetails userDetails =
+                        principalDetailsService.loadUserByUsername(userId.toString());
+
+                if (userDetails != null) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails, "" + userDetails.getAuthorities());
+                    SecurityContextHolder.getContext()
+                            .setAuthentication(usernamePasswordAuthenticationToken);
+                } else {
+                    throw new GlobalException(GlobalErrorCode.DEVELOPER_NOT_FOUND);
+                }
+            } else {
+                throw new GlobalException(GlobalErrorCode.AUTH_INVALID_TOKEN);
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
---
+++ src/main/java/ssuPlector/security/provider/JwtTokenProvider.java
@@ -0,0 +1,91 @@
+package ssuPlector.security.provider;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import ssuPlector.global.exception.GlobalException;
+import ssuPlector.global.response.code.GlobalErrorCode;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-validity}")
+    private long accessTokenValidityMilliseconds;
+
+    @Value("${jwt.refresh-token-validity}")
+    private long refreshTokenValidityMilliseconds;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createAccessToken(Long userId) {
+        return createToken(userId, accessTokenValidityMilliseconds);
+    }
+
+    public String createRefreshToken(Long userId) {
+        return createToken(userId, refreshTokenValidityMilliseconds);
+    }
+
+    public String createToken(Long userId, long validityMilliseconds) {
+        Claims claims = Jwts.claims();
+        claims.put("id", userId);
+
+        Date now = new Date();
+        Date expiredDate = new Date(now.getTime() + validityMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiredDate)
+                .signWith(this.getSigningKey())
+                .compact();
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            Jws<Claims> claims = getClaims(token);
+            Date now = new Date();
+            Date expiredDate = claims.getBody().getExpiration();
+            return expiredDate.after(now);
+        } catch (ExpiredJwtException e) {
+            throw new GlobalException(GlobalErrorCode.AUTH_EXPIRED_TOKEN);
+        } catch (SecurityException | MalformedJwtException | IllegalArgumentException e) {
+            throw new GlobalException(GlobalErrorCode.AUTH_INVALID_TOKEN);
+        } catch (UnsupportedJwtException e) {
+            throw new GlobalException(GlobalErrorCode.UNSUPPORTED_TOKEN);
+        }
+    }
+
+    public Jws<Claims> getClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+    }
+
+    public Long getId(String token) {
+        return getClaims(token).getBody().get("userId", Long.class);
+    }
+}
