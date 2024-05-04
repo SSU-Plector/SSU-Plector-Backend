82d09bb65abc0ee7353c30ddf38938dfab78bdd4
chanmin-00 <jjw421412@naver.com>
2024-05-05 01:30:04 +0900

    :recycle: [Refactor] application.yml 수정

--- src/main/java/ssuPlector/controller/RootController.java
+++ src/main/java/ssuPlector/controller/RootController.java
@@ -1,7 +1,6 @@
 package ssuPlector.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
