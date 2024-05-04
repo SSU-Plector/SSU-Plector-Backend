5f8a8ceb253bea65ae6cc6778b1dac9a2132a3fb
chanmin-00 <jjw421412@naver.com>
2024-05-04 14:56:17 +0900

    :recycle: [Refactor] CI/CD 버그 발생 수정

--- src/main/java/ssuPlector/controller/RootController.java
+++ src/main/java/ssuPlector/controller/RootController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/")
 @Tag(name = "Root", description = "루트 API")
 public class RootController {
 
