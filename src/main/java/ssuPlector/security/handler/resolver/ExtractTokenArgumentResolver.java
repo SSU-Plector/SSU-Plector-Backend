a7680dcff457f0a6e4e297b5158b4348dda37bea
88dldl <chltnrgml0999@gmail.com>
2024-05-09 13:02:20 +0900

    :sparkles: [Feat] 토큰 추출 어노테이션 추가

---
+++ src/main/java/ssuPlector/security/handler/resolver/ExtractTokenArgumentResolver.java
@@ -0,0 +1,35 @@
+package ssuPlector.security.handler.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import lombok.RequiredArgsConstructor;
+import ssuPlector.security.provider.JwtTokenProvider;
+
+@Component
+@RequiredArgsConstructor
+public class ExtractTokenArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(String.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory) {
+
+        String refreshToken = webRequest.getHeader("Authorization");
+        jwtTokenProvider.isValidToken(refreshToken);
+        return refreshToken;
+    }
+}
