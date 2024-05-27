d644ac1ba97cebe4502a5d200f60b83bb3b31e59
88dldl <chltnrgml0999@gmail.com>
2024-05-27 22:56:00 +0900

    :recycle: [Refactor] 카카오 서버에게 토큰 요청하는 과정 삭제 및 액세스 토큰으로 로그인처리

--- src/main/java/ssuPlector/security/SecurityConfig.java
+++ src/main/java/ssuPlector/security/SecurityConfig.java
@@ -62,7 +62,6 @@ public class SecurityConfig {
                                         "/swagger-resources/**",
                                         "/v3/api-docs/**",
                                         "/api/auth/kakao/login",
-                                        "/api/auth/kakao/callback",
                                         "api/developers/{developerId}",
                                         "/api/developers/list",
                                         "/api/projects/{projectId}",
