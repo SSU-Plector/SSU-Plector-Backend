ccbb0f396ea0a2e76338b4347990760ac73fb1cd
Ares-10 <cholionking1@naver.com>
2024-05-30 22:26:15 +0900

    feat: 더미 개발자 생성 api

--- src/main/java/ssuPlector/service/developer/DeveloperServiceImpl.java
+++ src/main/java/ssuPlector/service/developer/DeveloperServiceImpl.java
@@ -2,24 +2,32 @@ package ssuPlector.service.developer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
+import ssuPlector.aws.s3.AmazonS3Manager;
+import ssuPlector.converter.ImageConverter;
 import ssuPlector.domain.Developer;
+import ssuPlector.domain.Image;
+import ssuPlector.domain.Uuid;
 import ssuPlector.domain.category.DevLanguage;
 import ssuPlector.domain.category.DevTools;
 import ssuPlector.domain.category.TechStack;
+import ssuPlector.dto.request.DeveloperDTO;
 import ssuPlector.dto.request.DeveloperDTO.DeveloperListRequestDTO;
 import ssuPlector.dto.request.DeveloperDTO.DeveloperRequestDTO;
 import ssuPlector.dto.request.DeveloperDTO.DeveloperUpdateRequestDTO;
 import ssuPlector.global.exception.GlobalException;
 import ssuPlector.global.response.code.GlobalErrorCode;
 import ssuPlector.redis.service.DeveloperHitsService;
+import ssuPlector.repository.UuidRepository;
 import ssuPlector.repository.developer.DeveloperRepository;
 import ssuPlector.service.BaseMethod;
 
@@ -29,6 +37,8 @@ public class DeveloperServiceImpl implements DeveloperService {
     private final DeveloperRepository developerRepository;
     private final DeveloperHitsService developerHitsService;
     private final BaseMethod baseMethod;
+    private final AmazonS3Manager s3Manager;
+    private final UuidRepository uuidRepository;
 
     @Override
     @Transactional
@@ -98,5 +108,44 @@ public class DeveloperServiceImpl implements DeveloperService {
         Pageable pageable = PageRequest.of(page, 10);
         return developerRepository.findDevelopers(
                 requestDTO.getSortType(), requestDTO.getPart(), pageable);
+    }
+
+    @Override
+    @Transactional
+    public Long createDummyDeveloper(
+            DeveloperDTO.DummyDeveloperRequestDTO requestDTO, MultipartFile image) {
+        if (developerRepository.findByEmail(requestDTO.getEmail()).isPresent())
+            throw new GlobalException(GlobalErrorCode.DEVELOPER_DUPLICATE);
+
+        ArrayList<DevLanguage> newLanguage = baseMethod.fillList(requestDTO.getLanguageList());
+        ArrayList<DevTools> newDevTool = baseMethod.fillList(requestDTO.getDevToolList());
+        ArrayList<TechStack> newTechStack = baseMethod.fillList(requestDTO.getTechStackList());
+
+        Developer dummyDeveloper =
+                Developer.builder()
+                        .name(requestDTO.getName() + "_dummy")
+                        .email(requestDTO.getEmail())
+                        .shortIntro(requestDTO.getShortIntro())
+                        .university(requestDTO.getUniversity())
+                        .major(requestDTO.getMajor())
+                        .studentNumber(requestDTO.getStudentNumber())
+                        .kakaoId(requestDTO.getKakaoId())
+                        .githubLink(requestDTO.getGithubLink())
+                        .part1(requestDTO.getPart1())
+                        .part2(requestDTO.getPart2())
+                        .languageList(newLanguage)
+                        .devToolList(newDevTool)
+                        .techStackList(newTechStack)
+                        .build();
+
+        String uuid = UUID.randomUUID().toString();
+        Uuid savedUuid = uuidRepository.save(Uuid.builder().uuid(uuid).build());
+
+        String developerImageUrl =
+                s3Manager.uploadFile(s3Manager.generateProjectKeyName(savedUuid), image);
+        Image developerImage = ImageConverter.toImage(developerImageUrl);
+        dummyDeveloper.addImage(developerImage);
+
+        return developerRepository.save(dummyDeveloper).getId();
     }
 }
